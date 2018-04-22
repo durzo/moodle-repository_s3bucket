3d07f2c896a7a95765d924ae63c6487f39d6b74c
eWallah <info@eWallah.net>
2018-04-22 07:34:46 +0200

    travis modified

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2018042200;
-$plugin->requires  = 2017110800;
+$plugin->requires  = 2017111300;
 $plugin->component = 'repository_s3bucket';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = '3.4+';
